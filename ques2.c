4f715448236aecd9c3898e3498c157c230b9f6e0
manasshekhawat095-jpg <manasshekhawat095@gmail.com>
2026-08-10 23:03:27 +0530

    Add files via upload
    Day1 codes

---
+++ ques2.c
@@ -0,0 +1,14 @@
+//Q2: Write a program to input two numbers and display their sum, difference, product, and quotient.
+#include<stdio.h>
+int main()
+{
+    int a,b,c,d,e,f;
+    printf("enter two numbers: ");
+    scanf("%d %d",&a,&b);
+    c=a+b;
+    d=a-b;
+    e=a*b;  
+    f=a/b;
+    printf("Sum=%d, Diff=%d, Product=%d, Quotient=%d",c,d,e,f);
+    return 0;
+}
