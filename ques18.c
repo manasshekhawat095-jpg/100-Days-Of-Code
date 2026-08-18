35460c88e22fa55b0721ba2cccaf8219f5d5c6b1
MANAS SINGH SHEKHAWAT <manasshekhawat095@gmail.com>
2026-08-18 23:18:30 +0530

    Add files via upload
    Day9

---
+++ ques18.c
@@ -0,0 +1,20 @@
+//Q18: Write a program that accepts a percentage (0-100) and assigns a grade based on the following criteria: 
+
+#include<stdio.h>
+int main() {
+    float percentage;
+    printf("Enter the percentage (0-100): ");
+    scanf("%f", &percentage);
+    if (percentage >= 90) {
+        printf("Grade: A\n");
+    } else if (percentage >= 80) {
+        printf("Grade: B\n");
+    } else if (percentage >= 70) {
+        printf("Grade: C\n");
+    } else if (percentage >= 60) {
+        printf("Grade: D\n");
+    } else {
+        printf("Grade: F\n");
+    }
+    return 0;
+}
