78534a035df8728393525c3f03d9aea23a9a196a
MANAS SINGH SHEKHAWAT <manasshekhawat095@gmail.com>
2026-08-22 23:16:52 +0530

    Add files via upload
    Day13

---
+++ ques25.c
@@ -0,0 +1,48 @@
+//Q25: Write a program to implement a basic calculator using switch-case for +, -, *, /, %.
+
+#include<stdio.h>
+int main()
+{
+    char operator;
+    double num1, num2;
+
+    printf("Enter an operator (+, -, *, /, %): ");
+    scanf(" %c", &operator);
+
+    printf("Enter two numbers: ");
+    scanf("%lf %lf", &num1, &num2);
+
+    switch(operator)
+    {
+        case '+':
+            printf("%.2lf + %.2lf = %.2lf\n", num1, num2, num1 + num2);
+            break;
+
+        case '-':
+            printf("%.2lf - %.2lf = %.2lf\n", num1, num2, num1 - num2);
+            break;
+
+        case '*':
+            printf("%.2lf * %.2lf = %.2lf\n", num1, num2, num1 * num2);
+            break;
+
+        case '/':
+            if(num2 != 0)
+                printf("%.2lf / %.2lf = %.2lf\n", num1, num2, num1 / num2);
+            else
+                printf("Error! Division by zero.\n");
+            break;
+
+        case '%':
+            if((int)num2 != 0)
+                printf("%d %% %d = %d\n", (int)num1, (int)num2, (int)num1 % (int)num2);
+            else
+                printf("Error! Division by zero.\n");
+            break;
+
+        default:
+            printf("Error! Operator is not correct.\n");
+    }
+
+    return 0;
+}
