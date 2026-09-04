7b14cc60a28136eff72970642a5edb1a64c40e9c
MANAS SINGH SHEKHAWAT <manasshekhawat095@gmail.com>
2026-09-04 23:57:21 +0530

    Add files via upload
    Day26

---
+++ ques51.c
@@ -0,0 +1,17 @@
+//Q51: Write a program to print the following pattern:
+    
+
+#include <stdio.h>
+int main() {
+    int i, j, n = 5;
+    for(i = 1; i <= n; i++) {
+        for(j = 1; j <= n - i; j++) {
+            printf(" ");
+        }
+        for(j = 1; j <= i; j++) {
+            printf("%d", n - j + 1);
+        }
+        printf("\n");
+    }
+    return 0;
+}
